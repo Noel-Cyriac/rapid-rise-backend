021055be02feb02526c337c3814f03f4cdb06bc4
Noel Cyriac <noelcyriac99@gmail.com>
2026-02-24 11:10:09 +0530

    initial task1 setup

---
+++ task 1/task1/src/main/java/com/nc/ArithmeticApiApplication.java
@@ -0,0 +1,12 @@
+package com.nc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmeticApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArithmeticApiApplication.class, args);
+    }
+}
